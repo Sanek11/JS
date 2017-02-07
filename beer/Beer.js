aa2428aa00bd166fec164e0495bad3282069b2b2
Sanek11 <shura.karetnikov.89@mail.ru>
2017-02-07 22:47:31 +0300

    исправлен вид вывода

--- beer/Beer.js
+++ beer/Beer.js
@@ -7,6 +7,7 @@ console.log("Take one down, pass it around,");
 count = count - 1;
 if (count > 0) {
 console.log(count + " " + word + " of beer on the wall.");
+console.log("");
 } else {
 console.log("No more " + word + " of beer on the wall.");
 }
