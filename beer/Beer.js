8ae59747ff17966a6378aaeb685b44f794dc0e65
Daniel Kharchilin <kharchilindanila@gmail.com>
2017-02-15 21:18:53 +0300

    Update Beer.js

--- beer/Beer.js
+++ beer/Beer.js
@@ -1,14 +1,18 @@
-var word = "bottles";
-var count = 99;
+var word = "bottles",
+    count = 99;
+
 while (count > 0) {
     console.log(count + " " + word + " of beer on the wall");
     console.log(count + " " + word + " of beer,");
     console.log("Take one down, pass it around,");
+
     count = count - 1;
-    if (count > 0) {
-        console.log(count + " " + word + " of beer on the wall.");
-        console.log("");
-} else {
-    console.log("No more " + word + " of beer on the wall.");
+
+    if (count <= 0) {
+        console.log("No more " + word + " of beer on the wall.");
+        continue;
     }
+
+    console.log(count + " " + word + " of beer on the wall.");
+    console.log("");
 }
